a6adc9ecb72edc069a9ce7cb2ba5e1e8a5242309
KJ Monahan <kj@forumone.com>
2022-05-20 10:29:52 -0500

    [SLAC22-15] Add external and locked link markers

---
+++ web/themes/gesso/source/03-components/external-link/external-link.es6.js
@@ -0,0 +1,68 @@
+import Drupal from 'drupal';
+
+Drupal.behaviors.externalLinks = {
+  attach(context) {
+    const allowedDomains = [];
+    const lockedDomains = ['intranet.slac.stanford.edu'];
+    function linkIsExternal(linkElement) {
+      let isExternal = true;
+      if (
+        linkElement.host === 'www6.slac.stanford.edu' ||
+        linkElement.host.endsWith('.slac.stanford.edu') ||
+        linkElement.host === window.location.host
+      ) {
+        isExternal = false;
+      }
+
+      if (allowedDomains.length) {
+        allowedDomains.forEach(domain => {
+          if (
+            linkElement.host === domain ||
+            linkElement.host.endsWith(`.${domain}`)
+          ) {
+            isExternal = false;
+          }
+        });
+      }
+
+      return isExternal;
+    }
+
+    function linkIsLocked(linkElement) {
+      let isLocked = false;
+      if (lockedDomains.length) {
+        lockedDomains.forEach(domain => {
+          if (
+            linkElement.host === domain ||
+            linkElement.host.endsWith(`.${domain}`)
+          ) {
+            isLocked = true;
+          }
+        });
+      }
+      return isLocked;
+    }
+
+    const externalLinks = context.querySelectorAll(
+      "a:not([href=''], [href^='#'], [href^='?'], [href^='/'], [href^='.'], [href^='javascript:'], [href^='mailto:'], [href^='tel:'], .c-logo)"
+    );
+
+    externalLinks.forEach(el => {
+      if (el.hasAttribute('href')) {
+        if (linkIsLocked(el)) {
+          el.insertAdjacentHTML(
+            'beforeend',
+            `<svg class="c-icon" role="img"><title>(requires login)</title><use xlink:href="../images/sprite.artifact.svg#lock-solid"></use></svg>`
+          );
+          el.classList.add('external-link', 'external-link--locked');
+        } else if (linkIsExternal(el)) {
+          el.insertAdjacentHTML(
+            'beforeend',
+            `<svg class="c-icon" role="img"><title>(external link)</title><use xlink:href="../images/sprite.artifact.svg#diagonal-arrow"></use></svg>`
+          );
+          el.classList.add('external-link');
+        }
+      }
+    });
+  },
+};

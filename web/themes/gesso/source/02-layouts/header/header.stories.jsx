f8831d600165651928543dca7ad02a6ebec820f3
KJ Monahan <kj@forumone.com>
2022-05-23 14:20:29 -0500

    [SLAC22-16] Add mega menu on desktop

--- web/themes/gesso/source/02-layouts/header/header.stories.jsx
+++ web/themes/gesso/source/02-layouts/header/header.stories.jsx
@@ -1,24 +1,31 @@
 import parse from 'html-react-parser';
 
+import ReactDOMServer from 'react-dom/server';
+import React from 'react';
 import twigTemplate from './header.twig';
 import data from './header.yml';
+import { MegaMenu } from '../../03-components/mega-menu/mega-menu.stories';
 
 const settings = {
   title: 'Layouts/Header',
   argTypes: {
     is_demo: {
       table: {
-        disable: true
-      }
-    }
+        disable: true,
+      },
+    },
   },
 };
 
-const Header = args => (
-  parse(twigTemplate({
-    ...args,
-  }))
-);
+const Header = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      header_content: ReactDOMServer.renderToStaticMarkup(
+        <>{MegaMenu(MegaMenu.args)}</>
+      ),
+    })
+  );
 Header.args = { ...data };
 
 export default settings;

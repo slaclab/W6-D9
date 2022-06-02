4b830a0e2d34352ac09bd9e3cce01191aa373aaa
KJ Monahan <kj@forumone.com>
2022-06-02 10:34:00 -0500

    [SLAC22-37] Add basic section

--- web/themes/gesso/source/02-layouts/section/section.stories.jsx
+++ web/themes/gesso/source/02-layouts/section/section.stories.jsx
@@ -1,24 +1,41 @@
 import parse from 'html-react-parser';
 
+import ReactDOMServer from 'react-dom/server';
+import React from 'react';
 import twigTemplate from './section.twig';
+import gridTemplate from '../grid/grid.twig';
 import data from './section.yml';
+import { Default as Card } from '../../03-components/card/card.stories';
 
 const settings = {
   title: 'Layouts/Section',
   argTypes: {
     is_demo: {
       table: {
-        disable: true
-      }
-    }
+        disable: true,
+      },
+    },
   },
 };
 
-const Section = args => (
-  parse(twigTemplate({
-    ...args,
-  }))
-);
+const SectionContent = gridTemplate({
+  grid_content: ReactDOMServer.renderToStaticMarkup(
+    <>
+      {Card(Card.args)}
+      {Card(Card.args)}
+      {Card(Card.args)}
+    </>
+  ),
+  num_of_cols: 3,
+});
+
+const Section = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      section_content: SectionContent,
+    })
+  );
 Section.args = { ...data };
 
 export default settings;

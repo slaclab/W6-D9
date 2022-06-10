5e5da061dec083127f22bf3c1b95ca2d36b4514b
jack gold <jgold@forumone.com>
2022-06-10 12:28:38 -0400

    add basic click-to-hide behavior

--- web/themes/gesso/source/03-components/cookie-banner/cookie-banner.stories.jsx
+++ web/themes/gesso/source/03-components/cookie-banner/cookie-banner.stories.jsx
@@ -3,6 +3,7 @@ import parse from 'html-react-parser';
 import twigTemplate from './cookie-banner.twig';
 import data from './cookie-banner.yml';
 import './cookie-banner.scss';
+import './cookie-banner.es6';
 
 const settings = {
   title: 'Components/Cookie Banner'

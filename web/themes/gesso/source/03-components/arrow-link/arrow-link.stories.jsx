a2a23f5f9ebb80671c646020df0fe2b129ffdc11
KJ Monahan <kj@forumone.com>
2022-06-11 10:30:30 -0400

    [SLAC22-44] Create homepage tagline variation

---
+++ web/themes/gesso/source/03-components/arrow-link/arrow-link.stories.jsx
@@ -0,0 +1,19 @@
+import parse from 'html-react-parser';
+
+import twigTemplate from './arrow-link.twig';
+import data from './arrow-link.yml';
+
+const settings = {
+  title: 'Components/Arrow Link'
+};
+
+const ArrowLink = args =>
+  parse(
+    twigTemplate({
+      ...args,
+    })
+  );
+ArrowLink.args = { ...data };
+
+export default settings;
+export { ArrowLink };
---
+++ web/themes/gesso/source/03-components/tagline/tagline--long/tagline--long.stories.jsx
@@ -0,0 +1,20 @@
+import parse from 'html-react-parser';
+
+import twigTemplate from './tagline--long.twig';
+import data from './tagline--long.yml';
+import './tagline--long.scss';
+
+const settings = {
+  title: 'Components/Tagline/Long Tagline',
+};
+
+const TaglineLong = args =>
+  parse(
+    twigTemplate({
+      ...args,
+    })
+  );
+TaglineLong.args = { ...data };
+
+export default settings;
+export { TaglineLong };

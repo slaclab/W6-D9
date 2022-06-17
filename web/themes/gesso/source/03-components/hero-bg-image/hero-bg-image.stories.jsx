8fbbccec6426f342c0da227794fe46dda3250011
KJ Monahan <kj@forumone.com>
2022-06-17 14:15:36 -0500

    [SLAC22-64] Add Image Hero

--- web/themes/gesso/source/03-components/hero-bg-image/hero-bg-image.stories.jsx
+++ web/themes/gesso/source/03-components/hero-bg-image/hero-bg-image.stories.jsx
@@ -1,35 +1,21 @@
 import parse from 'html-react-parser';
 
 import twigTemplate from './hero-bg-image.twig';
+import globalData from '../../00-config/storybook.global-data.yml';
 import data from './hero-bg-image.yml';
 import './hero-bg-image.scss';
 
 const settings = {
-  title: 'Components/Hero/Hero with Background Image',
+  title: 'Components/Hero/Image Hero',
 };
 
-const Default = args => (
-  parse(twigTemplate({
-    ...args,
-  }))
-);
-Default.args = { ...data };
-
-const Left = args => (
-  parse(twigTemplate({
-    ...args,
-    modifier_classes: 'c-hero-bg-image--left',
-  }))
-);
-Left.args = { ...data };
-
-const Right = args => (
-  parse(twigTemplate({
-    ...args,
-    modifier_classes: 'c-hero-bg-image--right',
-  }))
-);
-Right.args = { ...data };
+const ImageHero = args =>
+  parse(
+    twigTemplate({
+      ...args,
+    })
+  );
+ImageHero.args = { ...globalData, ...data };
 
 export default settings;
-export { Default, Left, Right };
+export { ImageHero };

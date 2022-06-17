e6f41f7f3a7d5614c543e195b9564c701fb3d05f
KJ Monahan <kj@forumone.com>
2022-06-17 10:38:10 -0500

    [SLAC22-41] Add long version of timeline

--- web/themes/gesso/source/03-components/tagline/tagline--long/tagline--long.stories.jsx
+++ web/themes/gesso/source/03-components/tagline/tagline--long/tagline--long.stories.jsx
@@ -3,6 +3,7 @@ import parse from 'html-react-parser';
 import twigTemplate from './tagline--long.twig';
 import data from './tagline--long.yml';
 import './tagline--long.scss';
+import './tagline--long.es6';
 
 const settings = {
   title: 'Components/Tagline/Long Tagline',

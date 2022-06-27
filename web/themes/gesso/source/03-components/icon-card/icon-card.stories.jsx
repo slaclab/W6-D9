a60eb585c98b81beccab3df74af6c9f79cfa2e44
KJ Monahan <kj@forumone.com>
2022-06-27 10:36:59 -0500

    [SLAC22-59] Create icon cards and add to demo homepage

--- web/themes/gesso/source/03-components/icon-card/icon-card.stories.jsx
+++ web/themes/gesso/source/03-components/icon-card/icon-card.stories.jsx
@@ -2,9 +2,11 @@ import parse from 'html-react-parser';
 
 import twigTemplate from './icon-card.twig';
 import data from './icon-card.yml';
+import './icon-card.es6';
+import './icon-card.scss';
 
 const settings = {
-  title: 'Components/Icon Card'
+  title: 'Components/Icon Card',
 };
 
 const IconCard = args =>

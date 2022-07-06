bb5e7feaf8e084e1f83535da87eed6f9e74f93f7
jack gold <jgold@forumone.com>
2022-07-06 14:49:42 -0400

    event detail template

--- web/themes/gesso/source/04-templates/event-detail/event-detail.stories.jsx
+++ web/themes/gesso/source/04-templates/event-detail/event-detail.stories.jsx
@@ -7,6 +7,43 @@ import data from './event-detail.yml';
 import tagData from '../../03-components/tag-list/tag-list.yml';
 import mapData from '../../03-components/map/map.yml';
 import { EventDetails } from '../../03-components/event-details/event-details.stories';
+import { FiftyFifty } from '../../03-components/fifty-fifty/fifty-fifty.stories';
+import { Default as Figure } from '../../03-components/figure/figure.stories';
+import { Chevron } from '../../03-components/button/button.stories';
+
+const visitingText = `
+  <p class="c-kicker">Attending a public event</p>
+  <h2>Coming to SLAC</h2>
+  <p><b>Free Admission</b></p>
+  All lectures are free, registration is no longer required.</p><br>
+  <p><b>Identification Required</b></p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas rhoncus consequat sagittis. </p><br>
+  <p><b>Limited Seating</b></p>
+  Sed vulputate ligula quis tortor lobortis accumsan. Pellentesque viverra viverra ex nec vestibulum.</p><br>
+  <p>Nunc interdum lacinia felis ultricies sodales. Curabitur tortor tortor, commodo a porta et, dignissim a nunc. Integer efficitur eleifend posuere.</p>
+  <p>Cras dui lectus, aliquam posuere sagittis a, sollicitudin ac nunc. Etiam finibus urna et arcu venenatis, sit amet molestie sapien laoreet. Nam maximus lectus non augue faucibus, eget iaculis ligula ultricies. Nam sed libero augue. Aenean sed tempor dui. Etiam dignissim erat ac nibh tempus, non aliquet ipsum semper. Nulla venenatis bibendum purus id varius. Proin ligula orci, lobortis et tristique aliquet, sodales quis odio.</p>`;
+
+// const visitingButtons = ReactDOMServer.renderToStaticMarkup(<>{Button({
+//   modifier_classes: 'c-button--chevron',
+// })}</>);
+
+const visitingButton1 = ReactDOMServer.renderToStaticMarkup(<>{Chevron({
+  is_demo: false,
+  text: 'Site Entry Information',
+  url: '#'
+})}</>);
+
+const visitingButton2 = ReactDOMServer.renderToStaticMarkup(<>{Chevron({
+  is_demo: false,
+  text: 'Maps & Directions',
+  url: '#'
+})}</>);
+
+const fargs = {
+  col_1: ReactDOMServer.renderToStaticMarkup(<>{Figure({...Figure.args, caption: false})}</>),
+  col_2: visitingText + visitingButton1 + visitingButton2,
+};
+
 
 const settings = {
   title: 'Templates/Event Detail'
@@ -24,6 +61,7 @@ EventDetail.args = {
   map_iframe: mapData.map_iframe,
   related_topics: tagData.items,
   details: ReactDOMServer.renderToStaticMarkup(<>{EventDetails(EventDetails.args)}</>),
+  visiting: ReactDOMServer.renderToStaticMarkup(<>{FiftyFifty(fargs)}</>),
 };
 
 export default settings;

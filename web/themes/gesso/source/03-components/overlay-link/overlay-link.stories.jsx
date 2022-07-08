a9296bfdeffa4f87dac7b7d6f6311a7eb64e5fa4
jack gold <jgold@forumone.com>
2022-07-08 16:20:20 -0400

    91 & 56 - topic & poster FED

---
+++ web/themes/gesso/source/03-components/overlay-link/overlay-link.stories.jsx
@@ -0,0 +1,55 @@
+import parse from 'html-react-parser';
+
+import twigTemplate from './overlay-link.twig';
+import data from './overlay-link.yml';
+import globalData from '../../00-config/storybook.global-data.yml';
+
+const settings = {
+  title: 'Components/Overlay Link',
+  parameters: {
+    controls: {
+      include: [
+        'modifier_classes',
+        'image',
+        'label',
+        'kicker',
+        'title',
+      ],
+    },
+  },
+};
+
+const OverlayLink = args =>
+  parse(
+    twigTemplate({
+      ...args,
+    })
+  );
+OverlayLink.args = { ...data, ...globalData };
+
+const Poster = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      image: 'https://picsum.photos/390/600?image=237"',
+      label: '',
+      kicker: 'Poster Details',
+      title: 'Arianna Gleason lecture',
+    })
+  );
+Poster.args = { ...data, ...globalData };
+
+const Topic = args =>
+  parse(
+    twigTemplate({
+      ...args,
+      image: 'https://picsum.photos/400/400?image=237"',
+      label: 'Dark Energy',
+      kicker: 'Tagged in',
+      title: 'Dark Energy',
+    })
+  );
+Topic.args = { ...data, ...globalData };
+
+export default settings;
+export { OverlayLink, Poster, Topic };

2aa8a780b12076c35377fe8daea22ad911698b94
KJ Monahan <kj@forumone.com>
2022-07-19 13:44:47 -0500

    [SLAC22-273] Fix collision between topic grid and carousel JS

--- web/themes/gesso/source/03-components/topic-grid/topic-grid.es6.js
+++ web/themes/gesso/source/03-components/topic-grid/topic-grid.es6.js
@@ -5,26 +5,28 @@ import Packery from 'isotope-packery';
 import imagesLoaded from 'imagesloaded';
 import Drupal from 'drupal';
 
-Drupal.behaviors.carousel = {
+Drupal.behaviors.topicGrid = {
   attach(context) {
     const grid = context.querySelector('.c-topic-grid');
-    const iso = new Isotope(grid, {
-      layoutMode: 'packery',
-      itemSelector: '.c-topic-grid__item',
-      percentPosition: true,
-      packery: {
-        columnWidth: '.c-topic-grid__sizer',
-        gutter: '.c-topic-grid__gutter',
-      }
-    });
+    if (grid) {
+      const iso = new Isotope(grid, {
+        layoutMode: 'packery',
+        itemSelector: '.c-topic-grid__item',
+        percentPosition: true,
+        packery: {
+          columnWidth: '.c-topic-grid__sizer',
+          gutter: '.c-topic-grid__gutter',
+        },
+      });
 
-    const filterSelect = context.querySelector('.c-topic-grid__select');
-    filterSelect.addEventListener('change', function filterGrid() {
-      iso.arrange({ filter: this.value });
-    });
+      const filterSelect = context.querySelector('.c-topic-grid__select');
+      filterSelect.addEventListener('change', function filterGrid() {
+        iso.arrange({ filter: this.value });
+      });
 
-    imagesLoaded(grid).on('progress', () => {
-      iso.layout();
-    });
+      imagesLoaded(grid).on('progress', () => {
+        iso.layout();
+      });
+    }
   },
 };

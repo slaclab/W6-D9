199eeda55ec9fb4191933d05905211c559b5c371
jack gold <jgold@forumone.com>
2022-07-20 17:13:26 -0400

    eslint

--- web/themes/gesso/source/03-components/embed/embed.es6.js
+++ web/themes/gesso/source/03-components/embed/embed.es6.js
@@ -2,10 +2,10 @@ import Drupal from 'drupal';
 
 Drupal.behaviors.embed = {
   attach(context) {
-    let iframes = Array.from(context.getElementsByTagName('iframe'));
+    const iframes = Array.from(context.getElementsByTagName('iframe'));
 
     iframes.forEach(iframe => {
-      let ratio = iframe.width / iframe.height;
+      const ratio = iframe.width / iframe.height;
 
       iframe.style.aspectRatio = ratio;
       iframe.removeAttribute('height');

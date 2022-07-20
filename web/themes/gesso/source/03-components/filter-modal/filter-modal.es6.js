e9d13d3c8bf1fbfc49463d141d66e9d2869bf580
jack gold <jgold@forumone.com>
2022-07-20 12:27:15 -0400

    js tweaks

--- web/themes/gesso/source/03-components/filter-modal/filter-modal.es6.js
+++ web/themes/gesso/source/03-components/filter-modal/filter-modal.es6.js
@@ -4,7 +4,7 @@ Drupal.behaviors.filterModal = {
   attach(context) {
     const modalOuter = context.querySelector('.c-filter-modal');
     const modalInner = context.querySelector('.c-filter-modal__inner');
-    const modalClose = modalOuter.querySelector('.js-lightbox__close');
+    const modalClose = modalOuter && modalOuter.querySelector('.js-lightbox__close');
 
     if (!modalOuter || !modalInner || !modalClose) {
       return;
--- web/themes/gesso/source/03-components/topic-grid/topic-grid.es6.js
+++ web/themes/gesso/source/03-components/topic-grid/topic-grid.es6.js
@@ -8,27 +8,25 @@ import Drupal from 'drupal';
 Drupal.behaviors.carousel = {
   attach(context) {
     const grid = context.querySelector('.c-topic-grid');
-    if (!grid) {
-      return;
+    if (grid) {
+      const iso = new Isotope(grid, {
+        layoutMode: 'packery',
+        itemSelector: '.c-topic-grid__item',
+        percentPosition: true,
+        packery: {
+          columnWidth: '.c-topic-grid__sizer',
+          gutter: '.c-topic-grid__gutter',
+        }
+      });
+
+      const filterSelect = context.querySelector('.c-topic-grid__select');
+      filterSelect.addEventListener('change', function filterGrid() {
+        iso.arrange({ filter: this.value });
+      });
+
+      imagesLoaded(grid).on('progress', () => {
+        iso.layout();
+      });
     }
-
-    const iso = new Isotope(grid, {
-      layoutMode: 'packery',
-      itemSelector: '.c-topic-grid__item',
-      percentPosition: true,
-      packery: {
-        columnWidth: '.c-topic-grid__sizer',
-        gutter: '.c-topic-grid__gutter',
-      }
-    });
-
-    const filterSelect = context.querySelector('.c-topic-grid__select');
-    filterSelect.addEventListener('change', function filterGrid() {
-      iso.arrange({ filter: this.value });
-    });
-
-    imagesLoaded(grid).on('progress', () => {
-      iso.layout();
-    });
   },
 };

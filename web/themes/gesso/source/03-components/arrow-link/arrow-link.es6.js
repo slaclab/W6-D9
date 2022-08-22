8e16cf2a34294d81b901ebc8ddc1d7c44c8344df
jack gold <jgold@forumone.com>
2022-08-22 12:02:34 -0400

    fix chevrons

--- web/themes/gesso/source/03-components/arrow-link/arrow-link.es6.js
+++ web/themes/gesso/source/03-components/arrow-link/arrow-link.es6.js
@@ -3,7 +3,7 @@ import Drupal from 'drupal';
 Drupal.behaviors.arrowLink = {
   attach(context) {
     const arrowLinks = context.querySelectorAll(
-      '.c-arrow-link, .c-arrow-link--white'
+      '.c-arrow-link, .c-arrow-link--white, .c-card--small-bio__link'
     );
     arrowLinks.forEach(link => {
       const text = link.innerText.split(' ');

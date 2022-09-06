9cfff82fcf04af7610c88297e7d7e625289f574a
jack gold <jgold@forumone.com>
2022-09-06 17:27:48 -0400

    101 - embed integration

--- web/themes/gesso/source/03-components/embed/embed.es6.js
+++ web/themes/gesso/source/03-components/embed/embed.es6.js
@@ -2,16 +2,21 @@ import Drupal from 'drupal';
 
 Drupal.behaviors.embed = {
   attach(context) {
+    console.log('hello!!');
     const iframes = Array.from(context.querySelectorAll('.c-embed iframe'));
 
     iframes.forEach(iframe => {
       const parent = iframe.parentNode;
       const ratio = iframe.width / iframe.height;
 
+      console.log(ratio);
+
       const wrapper = document.createElement('div');
       wrapper.classList.add('c-embed__wrapper');
       wrapper.style.aspectRatio = ratio;
       wrapper.appendChild(iframe);
+
+      console.log(wrapper);
 
       iframe.style.aspectRatio = ratio;
       iframe.removeAttribute('height');

edbc4d9c9a495ff1ddc22e75a2908ee02790ed05
KJ Monahan <kj@forumone.com>
2022-10-05 14:15:14 -0400

    [SLAC22-492] Tweak script for arrow links

--- web/themes/gesso/source/03-components/arrow-link/arrow-link.es6.js
+++ web/themes/gesso/source/03-components/arrow-link/arrow-link.es6.js
@@ -6,12 +6,16 @@ Drupal.behaviors.arrowLink = {
       '.c-arrow-link, .c-arrow-link--white, .c-card--small-bio a'
     );
     arrowLinks.forEach(link => {
-      const text = link.innerHTML.split(' ');
+      const text = link.textContent.trim().split(' ');
       const lastWord = text.pop();
-      const lastWordMarkup = lastWord
-        ? ` <span class="c-arrow-link__word">${lastWord}</span>`
-        : '';
-      link.innerHTML = `${text.join(' ')}${lastWordMarkup}`;
+      if (lastWord) {
+        const lastWordMarkup = `<span class="c-arrow-link__word">${lastWord}</span>`;
+        const lastIndex = link.innerHTML.lastIndexOf(lastWord);
+        link.innerHTML =
+          link.innerHTML.substring(0, lastIndex) +
+          lastWordMarkup +
+          link.innerHTML.substring(lastIndex + lastWord.length);
+      }
     });
   },
 };
